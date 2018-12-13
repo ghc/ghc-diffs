853ad69163ecf3a39284c28f771a7a1f17a4b821
Ben Gamari <ben@smart-cactus.org>
2018-12-13 16:03:21 -0500

    gitlab-ci/linters: Port check-cpp linter

--- .gitlab/linters/check-makefiles.py
+++ .gitlab/linters/check-makefiles.py
@@ -8,27 +8,12 @@ Encourage the use of `$(TEST_HC_OPTS_INTERACTIVE)` instead of
 forget one of those flags when adding a new test.
 """
 
-import sys
-import os
-import json
-import re
-import subprocess
-from linter import lint_failure
+from linter import run_linters, RegexpLinter
 
-failed = 0
-base_commit = sys.argv[1]
-head_commit = sys.argv[2]
+linters = [
+    RegexpLinter(r'--interactive',
+                 message = "Warning: Use `$(TEST_HC_OPTS_INTERACTIVE)` instead of `--interactive -ignore-dot-ghci -v0`.")
+]
 
-files = subprocess.check_output(['git', 'diff', '--name-only', base_commit, head_commit, '--', './testsuite'], encoding='UTF-8')
-for path in files.split('\n'):
-    if os.path.isfile(path):
-        with open(path) as f:
-            for lineno, line in enumerate(f):
-                if '--interactive' in line:
-                    failed = 1
-                    msg = "Warning: Use `$(TEST_HC_OPTS_INTERACTIVE)` instead of `--interactive -ignore-dot-ghci -v0`."
-                    lint_failure(file=path, line_no=lineno,
-                                 line_content=line[:-1],
-                                 message=msg)
-
-sys.exit(failed)
+if __name__ == '__main__':
+    run_linters(linters) #$, subdir='testsuite')
